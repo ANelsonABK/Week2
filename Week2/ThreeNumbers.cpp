7ae9b723bcaf82fa5d5251fd81e68876f8fe958e
ANelsonABK <109537277+AlexNNelson@users.noreply.github.com>
2022-07-19 16:24:48 -0400

    Uncommented ThreeNumbers.cpp

--- Week2/ThreeNumbers.cpp
+++ Week2/ThreeNumbers.cpp
@@ -1,25 +1,25 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main()
-//{
-//	// Prompt user for three numbers
-//	cout << "Please enter three numbers: ";
-//
-//	// Get three numbers from user
-//	int num1, num2, num3;
-//	cin >> num1;
-//	cin >> num2;
-//	cin >> num3;
-//
-//	// Add the numbers
-//	int totalSum = num1 + num2 + num3;
-//
-//	// Average the numbers
-//	float avg = totalSum / 3.0f;
-//
-//	// Display the sum and average
-//	cout << "Sum = " << totalSum << endl;
-//	cout << "Average = " << avg << endl;
-//}
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Prompt user for three numbers
+	cout << "Please enter three numbers: ";
+
+	// Get three numbers from user
+	int num1, num2, num3;
+	cin >> num1;
+	cin >> num2;
+	cin >> num3;
+
+	// Add the numbers
+	int totalSum = num1 + num2 + num3;
+
+	// Average the numbers
+	float avg = totalSum / 3.0f;
+
+	// Display the sum and average
+	cout << "Sum = " << totalSum << endl;
+	cout << "Average = " << avg << endl;
+}
