964d8652ae91b287ed03ebe88733b2b9e10d5057
ANelsonABK <109537277+AlexNNelson@users.noreply.github.com>
2022-07-20 12:30:25 -0400

    Fixed upper bound issue for do-while loop.

--- NumberFactors/NumberFactors.cpp
+++ NumberFactors/NumberFactors.cpp
@@ -6,12 +6,12 @@ using namespace std;
 int main()
 {
 	// Get a positive number from user
-	int num = 1;
+	int64_t num = 1;
 
 	do {
 		cout << "Enter a number between 1 and 2,147,483,647:" << endl;
 		cin >> num;
-	} while (num < 1);
+	} while (num < 1 || num > 2147483647);
 
 	// Find and print all factors of given number
 	int range = (int)sqrt(num);
