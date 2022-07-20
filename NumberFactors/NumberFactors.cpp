0bca79f4040c88cefee268f4e344e503c0ec5424
ANelsonABK <109537277+AlexNNelson@users.noreply.github.com>
2022-07-20 12:25:04 -0400

    Added NumberFactors.cpp

---
+++ NumberFactors/NumberFactors.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	// Get a positive number from user
+	int num = 1;
+
+	do {
+		cout << "Enter a number between 1 and 2,147,483,647:" << endl;
+		cin >> num;
+	} while (num < 1);
+
+	// Find and print all factors of given number
+	int range = (int)sqrt(num);
+
+	cout << "Finding all factors of " << num << "..." << endl;
+
+	for (int factor = 1; factor <= range; factor++)
+	{
+		if (num % factor == 0)
+		{
+			cout << factor << " , " << (num / factor) << endl;
+		}
+	}
+
+	cout << "Found all the factors of " << num << "!" << endl;
+}
